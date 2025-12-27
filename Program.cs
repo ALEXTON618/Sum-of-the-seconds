2bc077b6da413604052f687c5e2d4aa7b0939edd
ALEXTON618 <a.k.hristov9877@gmail.com>
2025-12-28 00:59:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿int firstTime = int.Parse(Console.ReadLine()); //35
+int secondTime = int.Parse(Console.ReadLine());//30
+int thirdTime = int.Parse(Console.ReadLine()); //25
+
+int totalTime = firstTime + secondTime + thirdTime;
+//90 sek = 1 min 30 sek
+
+int minutes = totalTime / 60;
+int seconds = totalTime % 60;
+
+if (seconds <= 9)
+{
+    Console.WriteLine($"{minutes}:0{seconds}");
+}
+else
+{
+    Console.WriteLine($"{minutes}:{seconds}");
+}
